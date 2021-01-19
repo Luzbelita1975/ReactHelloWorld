b1040c7c162ff4f0e409a1fe7ebe68040caf3ffe
Fabiola Benitez <lumediaweb@gmail.com>
2021-01-18 21:48:35 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+ 
+        <div className="container">
+      <h1 className="title">
+      Hello world <span aria-label="emoji" role="img">🔥</span>
+   </h1>
+  
+  </div>
+      
+        <p>
+          Primera pagina de React JS by Fabiola Benitez
+        </p>
+    
+    
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
